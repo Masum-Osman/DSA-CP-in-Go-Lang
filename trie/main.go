5d623fcd7e3b0a9b3247f2b5e18802939a535613
Masum_Osman <ogkmasum@gmail.com>
2022-09-15 16:29:20 +0600

    trie DS. Insert, Search

--- trie/main.go
+++ trie/main.go
@@ -24,11 +24,51 @@ func InitTrie() *Trie {
 func (t *Trie) InsertWord(w string) {
 	wordLength := len(w)
 	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			currentNode.children[charIndex] = &Node{}
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	currentNode.isEnd = true
+}
 
-	fmt.Println(wordLength, currentNode)
+func (t *Trie) Search(w string) bool {
+	wrodLength := len(w)
+	currentNode := t.root
+
+	for i := 0; i < wrodLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			return false
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	if currentNode.isEnd == true {
+		return true
+	}
+	return false
 }
 
 func main() {
 	goTrie := InitTrie()
-	fmt.Println(testTrie)
+
+	toAdd := []string{
+		"aragorn",
+		"aragon",
+		"argon",
+		"eragon",
+		"oregon",
+		"oregano",
+		"oreo",
+	}
+
+	for i := 0; i < len(toAdd); i++ {
+		goTrie.InsertWord(toAdd[i])
+	}
+
+	fmt.Println(goTrie.Search("oreo"))
+
+	fmt.Println(goTrie.root)
 }
